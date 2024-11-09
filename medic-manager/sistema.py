e495d496922670a870dcbc26e9924529abee19ec
Matheus <matheusfiedlercolla@gmail.com>
2024-11-09 17:28:44 -0300

    Banco de dados base interface login

--- medic-manager/sistema.py
+++ medic-manager/sistema.py
@@ -1,5 +1,17 @@
-from flask import Flask
+from flask import Flask,render_template
 from flask_login import LoginManager
 
 sistema = Flask(__name__)
+login_manager = LoginManager()
 
+@sistema.route("/")
+def login():
+    return render_template("login.html")
+
+
+@sistema.route("/login", methods = ["POST"])
+def sucesso():
+    return "<p>Ok</p>"
+
+
+sistema.run(debug = True)
